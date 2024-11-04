2ccef8bf125c0af564162783a4a9eb1df101a0a6
soon <parkjs5143@gmail.com>
2024-11-04 20:18:01 +0900

    💄 refactor: Update AlignElementButton style
    isSelected prop 제거

--- src/components/LayerPanel/Button/AlignElementButton.tsx
+++ src/components/LayerPanel/Button/AlignElementButton.tsx
@@ -1,20 +1,13 @@
 type AddElementButtonProps = {
   buttonName: string;
-  isSelected: boolean;
   onClick: () => void;
 };
 
-const AlignElementButton = ({
-  buttonName,
-  isSelected,
-  onClick,
-}: AddElementButtonProps) => {
+const AlignElementButton = ({ buttonName, onClick }: AddElementButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={`${
-        isSelected ? "text-black bg-gray-200" : "text-white bg-transparent"
-      } py-2 w-full rounded-full transition duration-200 ease-in-out hover:bg-slate-700 active:bg-slate-900 focus:outline-none`}
+      className="py-2 w-full rounded-full text-white transition duration-200 ease-in-out hover:bg-slate-700 active:bg-slate-900 focus:outline-none"
     >
       {buttonName}
     </button>
--- src/components/LayerPanel/LayerPanel.tsx
+++ src/components/LayerPanel/LayerPanel.tsx
@@ -7,8 +7,6 @@ const LayerPanel = () => {
   const {
     elements,
     selectedIds,
-    alignButtonType,
-    
     addElement,
     handleAlignElements,
     selectElement,
@@ -33,22 +31,18 @@ const LayerPanel = () => {
         <div className="flex flex-col items-center">
           <AlignElementButton
             buttonName="All Vertically"
-            isSelected={alignButtonType === "vertical"}
             onClick={() => handleAlignElements("vertical")}
           />
           <AlignElementButton
             buttonName="All Horizontally"
-            isSelected={alignButtonType === "horizontal"}
             onClick={() => handleAlignElements("horizontal")}
           />
           <AlignElementButton
             buttonName="Group Vertically"
-            isSelected={alignButtonType === "verticalGroup"}
             onClick={() => handleAlignElements("verticalGroup")}
           />
           <AlignElementButton
             buttonName="Group Horizontally"
-            isSelected={alignButtonType === "horizontalGroup"}
             onClick={() => handleAlignElements("horizontalGroup")}
           />
         </div>
